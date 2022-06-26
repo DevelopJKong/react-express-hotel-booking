3a30a6e5473b232b1fb0f0e5e6e4dfb65f206cf1
DevelopJKong <ybaram40@gmail.com>
2022-06-26 16:44:51 +0900

    api 연동 전 frontend

--- frontend/src/components/featured/Featured.jsx
+++ frontend/src/components/featured/Featured.jsx
@@ -29,39 +29,29 @@ const TitleContainer = styled.div`
 `;
 
 const Featured = () => {
-  const { data, loading, error } = useFetch(
-    "/hotels/countByCity?cities=berlin,madrid,londo"
-  );
-
   return (
     <Container>
-      {loading ? (
-        "Loading please wait"
-      ) : (
-        <>
-          <Item>
-            <Img src="https://source.unsplash.com/random/1" />
-            <TitleContainer>
-              <h1>Berlin</h1>
-              <p>{data[0]} properties</p>
-            </TitleContainer>
-          </Item>
-          <Item>
-            <Img src="https://source.unsplash.com/random/2" />
-            <TitleContainer>
-              <h1>Madrid</h1>
-              <p>{data[1]} properties</p>
-            </TitleContainer>
-          </Item>
-          <Item>
-            <Img src="https://source.unsplash.com/random/3" />
-            <TitleContainer>
-              <h1>Londo</h1>
-              <p>{data[2]} properties</p>
-            </TitleContainer>
-          </Item>
-        </>
-      )}
+      <Item>
+        <Img src="https://source.unsplash.com/random/1" />
+        <TitleContainer>
+          <h1>Berlin</h1>
+          <p>{data[0]} properties</p>
+        </TitleContainer>
+      </Item>
+      <Item>
+        <Img src="https://source.unsplash.com/random/2" />
+        <TitleContainer>
+          <h1>Madrid</h1>
+          <p>{data[1]} properties</p>
+        </TitleContainer>
+      </Item>
+      <Item>
+        <Img src="https://source.unsplash.com/random/3" />
+        <TitleContainer>
+          <h1>Londo</h1>
+          <p>{data[2]} properties</p>
+        </TitleContainer>
+      </Item>
     </Container>
   );
 };
